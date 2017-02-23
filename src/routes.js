66baa133aa43f6294bd17dc84ae3032e5742f883
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-02-23 12:06:11 -0500

    Added nested routes | `this.props.children` added to App

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,14 @@ import {Route, indexRoute} from 'react-router';
 
 import App from './components/app';
 
+const Greeting = () => {
+  return (
+    <div>Hi there...</div>
+  )
+}
+
 export default (
-  <Route path="/" component={App} />  
+  <Route path="/" component={App}>
+    <Route path="greet" component={Greeting} />
+  </Route>
 )
