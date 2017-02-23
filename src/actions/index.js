0fa3026d217c2d5abb2b0f52e555c5a5eaa31313
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-02-23 18:57:50 -0500

    Added LifeCycle PostsIndex and fetchPosts can be called

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
-const const FETCH_POSTS = 'FETCH_POSTS'
+const FETCH_POSTS = 'FETCH_POSTS'
+
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
 const API_KEY = '?key=@#$ASDZXC'
+
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
   return {
     type: FETCH_POSTS,
-    payload: request    
+    payload: request
   }
 }
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts, test} from '../actions/index';
 
-export default () => {
-  return (
-    <div>List of blog posts</div>
-  );
+class PostsIndex extends Component {
+  // Lifecycle called one time just before component is going to be rendered to DOM.
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+  render() {
+    if (!this.props) {
+      return (<div>Not all </div>);
+    }
+    console.log(this.props);
+    return (
+      <div>List of blog posts</div>
+    );
+  }
 };
+
+export default connect(null, {fetchPosts})(PostsIndex);
