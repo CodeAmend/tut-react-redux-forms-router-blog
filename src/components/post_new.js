9276a18743274592d62e787efda0fd9661090b19
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-02-24 13:54:27 -0500

    Added form validation | hasDanger & hasError helper functions

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -7,24 +7,44 @@ class PostNew extends Component {
   render() {
     const {handleSubmit} = this.props;
     const {title, categories, content} = this.props.fields;
+
+    function hasDanger(name) {
+      if(name.touched && name.invalid) {
+        return 'has-danger';
+      }
+      return ''
+    }
+    function hasError(name) {
+      return name.touched ? name.error : '';
+    }
+
     return (
 
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create A New Post</h3>
 
-        <div className="form-group">
+        <div className={`form-group ${hasDanger(title)}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
+          <div className="text-help">
+            {hasError(title)}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${hasDanger(categories)}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
+          <div className="text-help">
+            {hasError(categories)}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${hasDanger(content)}`}>
           <label>Content</label>
           <textarea className="form-control" {...content} />
+          <div className="text-help">
+            {hasError(content)}
+          </div>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
@@ -34,8 +54,26 @@ class PostNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {}
+
+  if(!values.title) {
+    errors.title = 'Enter a title!';
+  }
+
+  if(!values.categories) {
+    errors.categories = 'Enter some categories!';
+  }
+
+  if(!values.content) {
+    errors.content = 'Enter some content!';
+  }
+
+  return errors;
+}
 
 export default reduxForm({
   form: 'PostNewForm',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate
 }, null, {createPost})(PostNew);
